6810354c4923219798dbe0ef6a822222dcde2b6f
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-15 12:09:44 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
-
+    stop = 0
+    start = 10
+    while (start != stop):
+        print(start)
+        start -= 1
+    else:
+        print("Happy New Year!")
+        
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [int * int for int in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    stop = 100
+    start = 0
+    while (start != stop):
+        start += 1
+        if start % 3 == 0 and start % 5 == 0:
+            print("FizzBuzz")
+        elif start % 3 == 0:
+            print("Fizz")
+        elif start % 5 == 0:
+            print("Buzz")
+        else:    
+           print(start)
+    
